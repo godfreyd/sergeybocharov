114b1e14a6d9a71adda03c53cb8770d9126e22bb
Sergey Bocharov <sergei-b84@bk.ru>
2022-06-16 00:15:23 +0300

    Fixes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { cn } from '@bem-react/classname';
 import { CustomLink  } from '../CustomLink';
-// import { Button } from '@lookingschools/ui/Button/desktop/bundle';
 import './index.css';
 
 export const cnHeader = cn('Header');
@@ -15,11 +14,9 @@ export const Header = (props) => {
                 <ul className={cnHeader('List')}>
                     <li className={cnHeader('ListItem')}>
                         <CustomLink to="/">About me</CustomLink>
-                        {/* <Button theme="black" size="m" type="link" url="/">About me</Button> */}
                     </li>
                     <li className={cnHeader('ListItem')}>
                         <CustomLink to="/projects">Projects</CustomLink>
-                        {/* <Button theme="black" size="m" type="link" url="/projects">Projects</Button> */}
                     </li>
                 </ul>
            </nav>
